41c978f36a9d3f397d17363956495b049d881608
George.Moiseenko <moiseenkogeorge1995@gmail.com>
2016-03-17 19:44:38 +0300

    add a little bit tests

---
+++ Polynom.Tests/PolynomTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Polynom.Tests
+{
+    [TestFixture]
+    public class PolynomTest
+    {
+        [Test]
+        [TestCase(1,2,3,Result = "+2*x^0+4*x^1+6*x^2")]
+        [TestCase(1, 2, 3, 4, Result = "+2*x^0+4*x^1+6*x^2+4*x^3")]
+        public string Add_Test(params double[] coff)
+        {
+            Polynom pol1 = new Polynom(1, 2, 3);
+            Polynom pol2 = new Polynom(coff);
+            return (pol1 + pol2).ToString();
+        }
+
+        [Test]
+        [TestCase(1, 2, 3, Result = "")]
+        [TestCase(1, 2, 3, 4, Result = "-4*x^3")]
+        public string Substruct_Test(params double[] coff)
+        {
+            Polynom pol1 = new Polynom(1, 2, 3);
+            Polynom pol2 = new Polynom(coff);
+            return (pol1 - pol2).ToString();
+        }
+
+        [Test]
+        [TestCase(1, 2, 3, Result = "+1*x^0+4*x^1+10*x^2+12*x^3+9*x^4")]
+        [TestCase(1, 2, 3, 4, Result = "+1*x^0+4*x^1+10*x^2+16*x^3+17*x^4+12*x^5")]
+        public string Mul_Test(params double[] coff)
+        {
+            Polynom pol1 = new Polynom(1, 2, 3);
+            Polynom pol2 = new Polynom(coff);
+            return (pol1 * pol2).ToString();
+        }
+
+        [Test]
+        [TestCase(1,2,3,Result = true)]
+        public bool Equal_Test(params double[] coff)
+        {
+            Polynom pol1 = new Polynom(coff);
+            Polynom pol2 = new Polynom(coff);
+            return pol1 == pol2;
+        }
+    }
+}
---
+++ Polynom.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Polynom.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Polynom.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6bae84a4-a014-480b-88b9-b78c1e020c94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
