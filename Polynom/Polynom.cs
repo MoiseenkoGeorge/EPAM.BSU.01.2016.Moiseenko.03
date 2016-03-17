065db1f7375e015b95895dea9d086077a8487a0b
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-03-17 14:32:28 +0300

    initial Polynom

---
+++ Polynom/Polynom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynom
+{
+    public class Polynom
+    {
+        private double[] coefficients;
+        public Polynom(params double[] coefficient)
+        {
+            coefficients = new double[coefficient.Length];
+            coefficients = coefficient;
+        }
+    }
+}
